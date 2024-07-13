e119f339164143fbf2990e217f2b76a35c14ace8
kevinshehan <kevin.shehan30@gmail.com>
2024-07-13 18:09:03 +0530

    port updated

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ mongoose.connect(MONGODB_URI)
 // Nodemailer configuration
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD
